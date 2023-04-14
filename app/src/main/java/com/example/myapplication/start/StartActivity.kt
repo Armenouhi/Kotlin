4a3d233f59d35c0f185029398b1bf0f7d4eb3486
Armenouhi <40177133+Armenouhi@users.noreply.github.com>
2023-04-14 18:30:23 +0400

    cteate first kotlin app

---
+++ app/src/main/java/com/example/myapplication/start/StartActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.start
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class StartActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
